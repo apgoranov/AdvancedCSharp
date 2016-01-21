aa46ffd8e78f2a6907a0d6ae4b46d4118c7fdd22
agoranov <el_moro@abv.bg>
2016-01-21 11:03:22 +0200

    Homework, Practice and Exam Preparations

---
+++ AdvancedCSharp_Arrays/ConsoleApplication1/BaloonSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class BaloonSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 5, 6, 532, 23, 41, 412, 411, 4121, 5651, 867, 32532 };
+            int count = 1;
+            while (count != 0)
+            {
+                count = 0;
+                int temp;
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
